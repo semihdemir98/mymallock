1fdeb377ca44fbb379d26a99b52a268aedb9d464
semihdemir98 <sdemir1414@gmail.com>
2020-04-04 11:49:34 -0400

    myfree ve strateji

--- mymallock.c
+++ mymallock.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef enum
 {
@@ -27,53 +28,12 @@ typedef struct
 static Block *free_list = NULL;  /*start of the free list*/
 static Block *heap_start = NULL; /*head of allocated memory from sbrk */
 
-void *mymalloc(size_t size)
-{
-  static int firstcall = 1;
-  size = size - (size % 16) + 16;
-  if (firstcall)
-  {
-    //
-    firstcall = 0;
-    heap_start = sbrk(sizeof(Block) * 2 * 1024);
-    if (heap_start == NULL)
-    {
-      perror("sbrk(heap_start) didnt work");
-      return -1;
-    }
-    heap_start->info.size = 1024;
-    heap_start->info.isfree = 1;
-    heap_start->next = NULL;
-    //free list
-    free_list = (void *)heap_start + sizeof(Block) + heap_start->info.size;
-    free_list->info.size = 1024;
-    free_list->info.isfree = 1;
-
-    // free alana 2 blok split et , birisini free listeye ekle
-    // digerinin size kadar alandaki baslangic adresini return et.
-
-    if (strategy == bestFit)
-    {
-      return best_fit_operation(size);
-    }
-    else if (strategy == worstFit)
-    {
-      return worst_fit_operation(size);
-    }
-    else if (strategy == firstFit)
-    {
-      return first_fit_operation(size);
-    }
-    return NULL;
-  }
-}
-
 void *remove_from_freelist(Block *p)
 {
   if (free_list == NULL)
   {
     fprintf(stderr, "free_list is empty\n");
-    return;
+    return NULL;
   }
   else
   {
@@ -84,7 +44,9 @@ void *remove_from_freelist(Block *p)
     }
     temp->next = p->next;
   }
+  return;
 }
+
 void *add_to_freelist(Block *p)
 {
   if (p == NULL)
@@ -101,13 +63,23 @@ void *add_to_freelist(Block *p)
     p->next = free_list;
     free_list = p;
   }
+  return;
+}
+Block *split(Block *b, size_t size)
+{
+  int tempsize = b->info.size;
+  b->info.size = size;
+  Block *b_split = ((void *)b + b->info.size + sizeof(Block));
+  b_split->info.size = tempsize - size - sizeof(Block);
+  add_to_freelist(b_split);
+  return b;
 }
 void *first_fit_operation(size_t size)
 {
   Block *p_Free = free_list;
   while (p_Free->next != NULL &&
-         p_Free->info.size < size ||
-         p_Free->info.isfree == 0)
+         (p_Free->info.size < size ||
+         p_Free->info.isfree == 0))
   {
     p_Free = (void *)p_Free + sizeof(Block) + p_Free->info.size;
   }
@@ -132,7 +104,7 @@ void *best_fit_operation(size_t size)
     Block *temp = free_list;
     Block *best = free_list;
 
-    while(temp->next =! NULL){
+    while(temp->next != NULL){
       if(temp->info.size >= size &&
           temp->info.size <= best->info.size )
           {
@@ -167,6 +139,44 @@ void *worst_fit_operation(size_t size){
   else{
     split(worst,size);
   }
+  return;
+}
+void *mymalloc(size_t size)
+{
+  static int firstcall = 1;
+  size = size - (size % 16) + 16;
+  if (firstcall)
+  {
+    //
+    firstcall = 0;
+    heap_start = sbrk(sizeof(Block) * 2 * 1024);
+    if (heap_start == NULL)
+    {
+      perror("sbrk(heap_start) didnt work");
+      return NULL;
+    }
+    heap_start->info.size = 1024;
+    heap_start->info.isfree = 1;
+    free_list = heap_start;
+
+    if(free_list == NULL){
+      perror("no avaible space!");
+    }
+   
+    if (strategy == bestFit)
+    {
+      return best_fit_operation(size);
+    }
+    else if (strategy == worstFit)
+    {
+      return worst_fit_operation(size);
+    }
+    else if (strategy == firstFit)
+    {
+      return first_fit_operation(size);
+    }
+  }
+  return;
 }
 void *myfree(void *p)
 {
@@ -177,17 +187,40 @@ void *myfree(void *p)
   {
     header_free->info.size += next_free->info.size;
   }
+  Block *prev_free = (void*)header_free - sizeof(Block);//previous block
+  if(!prev_free->info.isfree){
+    prev_free->info.size += header_free->info.size + sizeof(Block);
+    header_free=prev_free;
+  }
+  return (void*)header_free;
 }
-Block *split(Block *b, size_t size)
-{
-  int tempsize = b->info.size;
-  b->info.size = size;
-  Block *b_split = ((void *)b + b->info.size + sizeof(Block));
-  b_split = tempsize - size - sizeof(Block);
-  add_to_freelist(b_split);
-  return b;
+
+int test(int fitStrategy){
+   /*  strategy = fitStrategy;
+    for(int i = 0; i < 32; i++){
+        size_t random = rand() % 128;
+        int *x = mymalloc(random);
+        if(x == NULL){
+            return -1;
+        }
+        *x = random;
+        printf("%d\n",*x);
+        myfree(x);
+    }
+    return 1; */
 }
-int test(int fitStrategy);
 int main()
 {
+   strategy = firstFit;
+    for(int i = 0; i < 32; i++){
+        size_t random = rand() % 128;
+        int *x = mymalloc(random);
+        if(x == NULL){
+            return -1;
+        }
+        *x = random;
+        printf("%d\n",*x);
+        myfree(x);
+    }
+    return 1;
 }
